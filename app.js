919b7a00c629c0f23938e25d2000a38cb96816cb
juanSebastian <js.barragan796@uniandes.edu.co>
2018-03-01 23:38:34 -0500

    final fix heroku

--- app.js
+++ app.js
@@ -40,8 +40,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-console.log(`Password generator listening on ${port}`);
-
 module.exports = app;
